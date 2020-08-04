409f3841ca0cf7a19297abec4d4f7e160b4f873d
Adrian Sabou <adrian.sabou@evozon.com>
2020-08-04 15:44:48 +0300

    Updates

---
+++ precache-manifest.6ebb877d635a49b2990d7f50f7946476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23268e600491dd835155e3c155ab430f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8d712730128b5efe6984",
+    "url": "/robofriends/static/css/main.e7da643b.chunk.css"
+  },
+  {
+    "revision": "cadb83d287e325f91aec",
+    "url": "/robofriends/static/js/2.fa1425df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.fa1425df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d712730128b5efe6984",
+    "url": "/robofriends/static/js/main.3534733b.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
